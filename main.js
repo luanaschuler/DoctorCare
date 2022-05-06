96d38925d462fe09bc748c63ac97281021b82683
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2022-05-06 16:51:05 -0300

    function to scroll page and active links

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButton();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  //check if section passed the line
+  //which data do i need? offSetTop
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  //sections limits
+  const sectionBoudaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active');
+  if (sectionBoudaries) {
+    menuElement.classList.add('active');
+
+  }
+
 }
 
 function showNavOnScroll() {
